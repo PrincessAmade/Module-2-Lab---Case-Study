2946a63b9f9c888b9465842516e43a2be340fb5a
Princess Amade <pamade@ivytech.edu>
2025-06-23 19:14:20 -0400

    Added my name

--- casestudy.py
+++ casestudy.py
@@ -1,3 +1,6 @@
+#Author: Amade Monisola
+# File Name: student_gpa_checker.py
+# Description: This app accepts student names and GPAs, and checks if each student qualifies for the Dean's List (GPA ≥ 3.5) or Honor Roll (GPA ≥ 3.25). The programstops when the user enters 'ZZZ' as the last name
 while True:
     lname = input("What is your last name (or enter 'ZZZ' to end program): ")
     if lname == "ZZZ":
@@ -10,7 +13,7 @@ while True:
         print("Congratulations "+fname +" " + lname + " you are a high achiever and are on the dean's list.")
     elif gpa >= 3.25:
         print("Congratulations "+fname +" " + lname + " you have made the honor roll")
-    elif gpa > 5.0 or gpa < 0.0:
+    elif gpa > 4.0 or gpa < 0.0:
         print("Enter a valid gpa.")    
     else:
         print("Your gpa has been recorded.")
